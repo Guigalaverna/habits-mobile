280ad6b1704c0b3207cd78a0d5942e8a38520b98
Guilherme Galaverna <guigalaverna@gmail.com>
2023-01-19 15:33:39 -0300

    feat: finish first class

--- App.tsx
+++ App.tsx
@@ -23,8 +23,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar translucent backgroundColor="transparent" />
     </View>
   );
 }
@@ -32,8 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#1a1a0a",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  text: {
+    color: "#fff",
   },
 });
